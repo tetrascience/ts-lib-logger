7b1d5aaef4633222e65318806946019668f8fbbe
Steve Berczuk <sberczuk@tetrascience.com>
2018-03-22 09:49:01 -0400

    IN-207 added functionality to allow enhanced console logging

--- test/console-logger.js
+++ test/console-logger.js
@@ -1,5 +1,8 @@
 "use strict";
 
+const chai = require('chai');
+const expect = chai.expect;
+require('mocha-sinon');
 
 describe('console-logger', function () {
 
@@ -21,4 +24,26 @@ describe('console-logger', function () {
         done();
     });
 
+
+
+});
+
+describe ('console-logger log method-with-defaults', function(){
+    beforeEach(function () {
+        this.sinon.spy(console, 'log');
+    });
+
+    it('should be able to log to console with level and timestamp info',function(done){
+        let logger = require('../')('console');
+        const timeStamp = "2018-03-21T22:27:03.085Z";
+        logger.log("message", 1, timeStamp);
+        const loggedMessage = console.log.getCall(0).args[0];
+        expect(loggedMessage).to.contain("message");
+        expect(loggedMessage).to.contain(timeStamp);
+        console.log(loggedMessage);
+
+        logger.info(1);
+        logger.info('some thing to notice');
+        done();
+    });
 });
